80be00113d5a3da3f0e4587fc5a11ac801880c1c
Mateus <mateusc.rossetto@hotmail.com>
2022-07-02 17:21:13 -0300

    feat: grafcs of solutions

--- app.py
+++ app.py
@@ -6,14 +6,85 @@ import matplotlib.pyplot as plt
 
 # definindo lambda1 = 2 e lamda2 = 4
 
-lamb_one = 2
-lamb_two = 4
+lamb_one = 4.56 * (10 ** (-3))
+lamb_two = 0.05
+
+delta_t = 0.0001
+final_interval_t = 330
+f0_one = 11.35
+f0_two = 0
+
+steps = np.arange(0, final_interval_t + delta_t, delta_t)
 
 
-def f_line_one(lamb, fx):
-    return - lamb * fx
+def f_line_one(fx):
+    return - lamb_one * fx
 
 
-def f_line_two(lamb_one, lamb_two, fx_one, fx_two):
+def f_line_two(fx_one, fx_two):
+    return lamb_one * fx_one - lamb_two * fx_two
 
-    return lamb_one * fx_two - fx_one * lamb_two
+
+def f_n2(t):
+    return lamb_one * f0_one * np.exp(-lamb_one * t) / (lamb_two)
+
+# f(x0 + ∆x) = f(x0) + f'(x0)∆x
+
+
+def euler_method(f0_one, f0_two, delta_x, steps, f_line_one, f_line_two):
+    results_one = [f0_one]
+    results_two = [f0_two]
+
+    fx_one = f0_one
+    fx_two = f0_two
+
+    for t in steps:
+        next_f_one = fx_one + f_line_one(fx_one) * delta_x
+        results_one.append(next_f_one)
+
+        next_f_two = fx_two + f_line_two(fx_one, fx_two) * delta_x
+        results_two.append(next_f_two)
+
+        fx_two = next_f_two
+        fx_one = next_f_one
+
+    results_one.pop()
+    results_two.pop()
+
+    return {'one': results_one, 'two': results_two}
+
+
+euler = euler_method(f0_one, f0_two, delta_t, steps, f_line_one, f_line_two)
+
+analytical = []
+for t in steps:
+    analytical.append(f_n2(t))
+analytical[0] = 0
+
+plt.plot(steps, euler['one'])
+plt.plot(steps, (euler['two']))
+plt.title('Decaimento Radioativo')
+plt.xlabel('Time')
+plt.ylabel('Massa do elemento')
+plt.legend(["Função 1 (n1)", "Função 2 (n2)"])
+plt.savefig("decaimento-radioativo.png", dpi=100)
+plt.show()
+
+
+# plt.plot(steps, euler_10)
+# plt.title('Decaimento Radioativo')
+# plt.xlabel('Time')
+# plt.ylabel('Massa do elemento')
+# plt.legend(["Função 1 (n1)", "Função 2 (n2)"])
+# plt.savefig("decaimento-radioativo-10n2.png", dpi=100)
+# plt.show()
+
+
+# plt.plot(steps, analytical)
+# plt.plot(steps, euler['two'])
+# plt.title('Decaimento Radioativo')
+# plt.xlabel('Time')
+# plt.ylabel('Massa do elemento')
+# plt.legend(["analitical", "Função 2 (n2)"])
+# plt.savefig("decaimento-radioativo-comparação-erro.png", dpi=100)
+# plt.show()
